89b689f944983e1fbf5c33ffb4359bf55e604248
50n1cd347h9 <8yzuie9g@duck.com>
2024-06-30 15:48:34 +0900

    hoge~

--- src/main.zig
+++ src/main.zig
@@ -6,11 +6,17 @@ const stdout = io.getStdOut().writer();
 const process = std.process;
 const print = std.debug.print;
 const eql = std.mem.eql;
+const pow = std.math.pow;
+const shr = std.math.shr;
 const ArrayList = std.ArrayList;
+const mem = std.mem;
+const copyForwards = mem.copyForwards;
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const builtin = std.builtin;
 
 // the order is matter
-const instructions = [_][]const u8{ "push", "pop", "add", "sub", "mul", "div", "and", "or", "xor", "shl", "ldr", "ldm", "cmp", "jmp", "jg", "jz", "jl", "nop" };
+// ldr, ldm
+const instructions = [_][]const u8{ "push", "pop", "add", "sub", "mul", "div", "and", "or", "xor", "shl", "ld", "ld", "cmp", "jmp", "jg", "jz", "jl", "nop" };
 const registers = [_][]const u8{
     "ip",
     "sp",
@@ -21,23 +27,40 @@ const registers = [_][]const u8{
 };
 
 const ImFineAssembly = struct {
-    source_file: std.fs.File,
+    src_file: std.fs.File,
+    dst_file: std.fs.File,
     input_reader: std.fs.File.Reader,
+    output_writer: std.fs.File.Writer,
     line_buffer: [0x100]u8,
     token: Token,
     curr_line_code: Code,
-    allocator: std.mem.Allocator,
+    codes: ArrayList(Code),
+
+    const COMMA: u8 = ',';
+    const SEMICOLON: u8 = ';';
+    const SPACE: u8 = ' ';
+    const SQUARE_BRACKET_OPEN = '[';
+    const SQUARE_BRACKET_CLOSE = ']';
+    const BC_imm: u2 = 0b00;
+    const BC_reg: u2 = 0b01;
+    const BC_imm_ref: u2 = 0b10;
+    const BC_reg_ref: u2 = 0b11;
 
     var str_BC_map: [instructions.len + registers.len]StrBc = undefined;
     var begin_idx: u32 = 0;
     var curr_idx: u32 = 0;
+    var allocator: std.mem.Allocator = undefined;
 
+    const SyntaxError = error{
+        CommaNeeded,
+    };
     const Token = enum {
         Register,
         Instruciton,
         Imm,
         Ref,
         InvalidToken,
+        End,
     };
     const StrBc = struct {
         name: []const u8,
@@ -47,45 +70,19 @@ const ImFineAssembly = struct {
         opcode: u8,
         ext: u2,
         len: u3,
-        first_oprand: u3,
+        first_oprand: u8,
         padding: u2,
         second_oprand: u128,
     };
 
-    const BC_push: u8 = 0x0;
-    const BC_pop: u8 = 0x1;
-    const BC_add: u8 = 0x2;
-    const BC_sub: u8 = 0x3;
-    const BC_mul: u8 = 0x4;
-    const BC_div: u8 = 0x5;
-    const BC_and: u8 = 0x6;
-    const BC_or: u8 = 0x7;
-    const BC_xor: u8 = 0x8;
-    const BC_shl: u8 = 0x9;
-    const BC_ldr: u8 = 0xa;
-    const BC_ldm: u8 = 0xb;
-    const BC_cmp: u8 = 0xc;
-    const BC_jmp: u8 = 0xd;
-    const BC_jg: u8 = 0xf;
-    const BC_jz: u8 = 0x10;
-    const BC_jl: u8 = 0x11;
-    const BC_nop: u8 = 0x12;
+    const Int = struct {
+        number: u128,
+        len: u16,
+    };
 
-    const BC_reg_ip: u8 = 0b000;
-    const BC_reg_sp: u8 = 0b001;
-    const BC_reg_fp: u8 = 0b010;
-    const BC_reg_flag: u8 = 0b011;
-    const BC_reg_gr0: u8 = 0b100;
-    const BC_reg_gr1: u8 = 0b101;
-
-    const COMMA: u8 = ',';
-    const SEMICOLON: u8 = ';';
-    const SPACE: u8 = ' ';
-    const SQUARE_BRACKET_OPEN = '[';
-    const SQUARE_BRACKET_CLOSE = ']';
-
-    fn init(self: *ImFineAssembly, allocator: std.mem.Allocator) !void {
-        self.allocator = allocator;
+    fn init(self: *ImFineAssembly, _allocator: std.mem.Allocator) !void {
+        _ = self;
+        allocator = _allocator;
         for (instructions, 0..) |instruction, i| {
             str_BC_map[i].name = instruction;
             str_BC_map[i].bc = @as(u8, @intCast(i));
@@ -96,6 +93,17 @@ const ImFineAssembly = struct {
         }
     }
 
+    fn emptyCode() Code {
+        return Code{
+            .opcode = 0,
+            .ext = 0,
+            .len = 0,
+            .first_oprand = 0,
+            .padding = 0,
+            .second_oprand = 0,
+        };
+    }
+
     fn getBc(bcmap: [*]StrBc, name: []const u8) u8 {
         for (0..str_BC_map.len) |i| {
             if (std.mem.eql(u8, bcmap[i].name, name))
@@ -104,11 +112,42 @@ const ImFineAssembly = struct {
         return 0;
     }
 
-    fn isSmallLetterOrDigit(char: u8) bool {
+    fn isLetterOrDigit(char: u8) bool {
         switch (char) {
-            'a'...'z', '0'...'9' => return true,
+            'a'...'z', 'A'...'Z', '0'...'9' => return true,
             else => return false,
         }
+    }
+
+    fn toInt(buf: []const u8) u128 {
+        var res: ?u128 = 0;
+        if (buf.len > 2 and buf[0] == '0' and buf[1] == 'x') {
+            res = std.fmt.parseInt(u128, buf[2..buf.len], 16) catch null;
+        } else {
+            res = std.fmt.parseInt(u128, buf, 10) catch null;
+        }
+        if (res != null)
+            return res.?;
+        return res.?;
+    }
+
+    fn isImm(buf: []const u8) bool {
+        var res: ?u128 = 0;
+        for ([2]u8{ 10, 16 }) |base| {
+            if (buf.len > 2 and buf[0] == '0' and buf[1] == 'x') {
+                res = std.fmt.parseInt(u128, buf[2..buf.len], base) catch null;
+            } else {
+                res = std.fmt.parseInt(u128, buf, base) catch null;
+            }
+            if (base == 16) {
+                if (res == null) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     fn isRegister(buf: []u8) bool {
@@ -125,49 +164,146 @@ const ImFineAssembly = struct {
         return false;
     }
 
+    fn hasComma(buf: []const u8) bool {
+        for (buf) |char| {
+            switch (char) {
+                SEMICOLON => return false,
+                COMMA => return true,
+                else => continue,
+            }
+        }
+        return false;
+    }
+
     fn nextChar(buf: []u8) u8 {
         return buf[0];
     }
 
-    fn nextToken(self: *ImFineAssembly) Token {
+    fn nextToken(self: *ImFineAssembly) !Token {
+        defer {
+            begin_idx = curr_idx;
+        }
         const buf_end = self.line_buffer.len;
         for (self.line_buffer[begin_idx..buf_end], 0..) |char, i| {
             switch (char) {
+                '\x00' => return Token.InvalidToken,
                 SPACE, COMMA => continue,
+                SEMICOLON => return Token.InvalidToken,
                 SQUARE_BRACKET_OPEN => {
                     begin_idx += @as(u32, @intCast(i));
                     curr_idx = begin_idx;
 
                     while (nextChar(self.line_buffer[curr_idx..buf_end]) != SQUARE_BRACKET_CLOSE)
                         curr_idx += 1;
+                    curr_idx += 1;
+
+                    const inner_str = self.line_buffer[begin_idx + 1 .. curr_idx - 1];
+
+                    if (isRegister(inner_str)) {
+                        // if opcode ld (ldm)
+                        if (self.token == Token.Instruciton) {
+                            self.curr_line_code.opcode += 1;
+                            self.curr_line_code.first_oprand = getBc(&str_BC_map, inner_str);
+                        } else {
+                            if (!hasComma(self.line_buffer[0..begin_idx]) and self.token != Token.Instruciton)
+                                return SyntaxError.CommaNeeded;
+                            self.curr_line_code.second_oprand = getBc(&str_BC_map, inner_str);
+                            self.curr_line_code.ext = BC_reg_ref;
+                        }
+                        return Token.Register;
+                    }
+
+                    if (isImm(inner_str)) {
+                        const int_num = toInt(inner_str);
+                        if (!hasComma(self.line_buffer[0..begin_idx]) and self.token != Token.Instruciton)
+                            return SyntaxError.CommaNeeded;
+                        self.curr_line_code.second_oprand = int_num;
+                        self.curr_line_code.ext = BC_imm_ref;
+                    }
                 },
                 else => {
                     begin_idx += @as(u32, @intCast(i));
                     curr_idx = begin_idx;
 
-                    while (isSmallLetterOrDigit(nextChar(self.line_buffer[curr_idx..buf_end]))) {
+                    while (isLetterOrDigit(nextChar(self.line_buffer[curr_idx..buf_end]))) {
                         curr_idx += 1;
                     }
-                    if (isOpcode(self.line_buffer[begin_idx..curr_idx])) {
-                        begin_idx = curr_idx;
+
+                    const token_str = self.line_buffer[begin_idx..curr_idx];
+
+                    if (isOpcode(token_str)) {
+                        self.curr_line_code.opcode = getBc(&str_BC_map, token_str);
                         return Token.Instruciton;
                     }
-                    //                    print("/hug{s}/\n", .{self.line_buffer[begin_idx..curr_idx]});
-                    if (isRegister(self.line_buffer[begin_idx..curr_idx])) {
-                        begin_idx = curr_idx;
+
+                    if (isRegister(token_str)) {
+                        if (hasComma(self.line_buffer[begin_idx..buf_end])) {
+                            self.curr_line_code.first_oprand = getBc(&str_BC_map, token_str);
+                        } else {
+                            if (!hasComma(self.line_buffer[0..begin_idx]) and self.token != Token.Instruciton)
+                                return SyntaxError.CommaNeeded;
+                            self.curr_line_code.second_oprand = getBc(&str_BC_map, token_str);
+                            self.curr_line_code.ext = BC_reg;
+                        }
                         return Token.Register;
                     }
+
+                    if (isImm(token_str)) {
+                        const int_num = toInt(token_str);
+                        self.curr_line_code.second_oprand = int_num;
+                        self.curr_line_code.ext = BC_imm;
+                    }
+
+                    return Token.InvalidToken;
                 },
             }
         }
         return Token.InvalidToken;
     }
 
+    fn toLSB(num: u128, index: usize) u8 {
+        const mask: u32 = 0xff;
+        const ofs: u5 = @intCast(index);
+        const byte = shr(u128, num, ofs * 8) & mask;
+        return @as(u8, @intCast(byte));
+    }
+
+    fn toBinary(self: *ImFineAssembly) !void {
+        var out_buf = ArrayList(u8).init(allocator);
+        defer out_buf.deinit();
+
+        for (self.codes.allocatedSlice()[0..self.codes.items.len]) |*code| {
+            code.len = switch (code.second_oprand) {
+                0 => 0b000,
+                1...(pow(u16, 2, 8) - 1) => 0b001,
+                pow(u16, 2, 8)...(pow(u32, 2, 16) - 1) => 0b010,
+                pow(u32, 2, 16)...(pow(u64, 2, 32) - 1) => 0b011,
+                pow(u64, 2, 32)...(pow(u128, 2, 64) - 1) => 0b100,
+                pow(u128, 2, 64)...(pow(u129, 2, 128) - 1) => 0b100,
+            };
+            const bytes: u8 = @divExact(pow(u8, 2, code.len + 2), 8);
+
+            try out_buf.append(code.opcode << 2 | code.ext);
+            try out_buf.append(code.opcode << 5 | code.first_oprand << 2 | code.padding);
+            for (0..bytes) |i| {
+                const byte = toLSB(code.second_oprand, i);
+                try out_buf.append(byte);
+            }
+
+            print("{!}\n", .{code});
+        }
+        stdout.print("{s}\n", .{out_buf.items}) catch unreachable;
+        //            self.output_writer.write(code.opcode << 5 | code.first_oprand << 2 | code.padding);
+    }
+
     fn nextLine(self: *ImFineAssembly) ?[]const u8 {
+        @memset(&self.line_buffer, 0);
         const res = self.input_reader.readUntilDelimiterOrEof(
             &self.line_buffer,
             '\n',
         ) catch null;
+        std.mem.replaceScalar(u8, &self.line_buffer, '\n', '\x00');
+
         if (res) |line| {
             _ = line;
         } else {
@@ -181,41 +317,44 @@ const ImFineAssembly = struct {
     }
 
     fn assemble(self: *ImFineAssembly, file_name: []const u8) !void {
-        self.source_file = try fs.cwd().openFile(
+        self.src_file = try fs.cwd().openFile(
             file_name,
             .{ .mode = .read_only },
         );
-        defer self.source_file.close();
-        self.input_reader = self.source_file.reader();
+        self.dst_file = try fs.cwd().createFile(
+            "hoge.bin",
+            .{},
+        );
+        defer self.src_file.close();
+        defer self.dst_file.close();
+        self.input_reader = self.src_file.reader();
+        self.output_writer = self.dst_file.writer();
+        self.codes = ArrayList(Code).init(allocator);
+        defer self.codes.deinit();
 
-        //print("{any}\n", .{str_BC_map});
-        print("{x}\n", .{getBc(&str_BC_map, "gr0")});
-        while (self.nextLine() != null) {
+        line: while (self.nextLine() != null) {
             curr_idx = 0;
             begin_idx = 0;
-            self.curr_line_code = undefined;
+            self.curr_line_code = emptyCode();
 
             token: while (true) {
-                self.token = self.nextToken();
+                self.token = try self.nextToken();
                 switch (self.token) {
                     Token.Instruciton => continue,
-                    Token.Register => break :token,
-                    Token.Imm => return,
-                    Token.Ref => return,
-                    else => break :token,
+                    Token.Register => continue,
+                    Token.Imm => continue,
+                    Token.Ref => continue,
+                    Token.InvalidToken => if (begin_idx == 0) {
+                        break :line;
+                    } else {
+                        try self.codes.append(self.curr_line_code);
+                        break :token;
+                    },
+                    else => break :line,
                 }
             }
-
-            break;
         }
-        // var buf_reader = std.io.bufferedReader(self.source_file.reader());
-        // const input_reader = buf_reader.reader();
-        // while (try input_reader.readUntilDelimiterOrEof(
-        //     &self.line_buffer,
-        //     '\n',
-        // )) |line| {
-        //     print("{s}\n", .{line});
-        // }
+        try self.toBinary();
     }
 };
 
--- test.asm
+++ test.asm
@@ -1,2 +1,7 @@
+ld [gr0], 0x10
 pop gr0
 push gr1
+ld gr0, [gr0]
+ld gr1, 0x100
+ld [gr1], [gr0]
+;ld [gr1]  [gr0]
