48c82faa07d559c6d73d0a8c01ecc48d7958d137
50n1cd347h9 <8yzuie9g@duck.com>
2025-02-28 13:51:53 +0000

    feat: read token and return token type

--- src/Assembler.zig
+++ src/Assembler.zig
@@ -20,99 +20,39 @@ const SignedByteWidth: type = @Type(.{ .int = .{
 } });
 const ImFineAssembler = @This();
 
-allocator: mem.Allocator,
-tokens: ArrayList(?[]u8),
-codes: ArrayList(Code),
-label_addr: ArrayList(Label2addr),
 src_name: []u8,
-dst_name: []u8,
-line_buffer: [0x100]u8,
-src_file_buf: [:0]u8,
-out_buf: ArrayList(u8),
-
-const COMMA: u8 = ',';
-const SEMICOLON: u8 = ';';
-const SPACE: u8 = ' ';
-const SQUARE_BRACKET_OPEN = '[';
-const SQUARE_BRACKET_CLOSE = ']';
-const INDENT = '\t';
-const COLON: u8 = ':';
-const EOL = '\x00';
-const LABEL: u8 = 0b11 << 6;
-const TOKEN_DELIMITER = '\x00';
-const LINE_DELIMITER = '\n';
-const COMMENT_START = '#';
-
-var DEBUG = false;
-
-const filename_len = 0x200;
-var file_name_buf = [_]u8{0} ** (filename_len);
-
-pub const SyntaxError = error{
-    CommaNeeded,
-    OpenSquareBracketNeeded,
-    CloseSquareBracketNeeded,
-    LabelNotFound,
-    UnknownToken,
-};
 
 const ArgError = error{
     FileNotFound,
     TooFewArgs,
 };
 
-pub const Code = struct {
-    opcode: u8,
-    ext: u2,
-    len: u3,
-    first_oprand: u8,
-    padding: u2,
-    second_oprand: u128,
-};
-
-pub const Label2addr = struct {
-    label_str: []u8,
-    label_idx: ByteWidth,
-    addr_abs: ByteWidth,
-};
-
-pub const LabelSlot = struct {
-    place: ByteWidth, // blank place that should be resolved
-    label_idx: ByteWidth,
-};
-
 pub fn init(allocator: mem.Allocator) ImFineAssembler {
-    @memset(&file_name_buf, 0);
+    _ = allocator;
 
     return ImFineAssembler{
-        .allocator = allocator,
         .src_name = undefined,
-        .dst_name = undefined,
-        .line_buffer = undefined,
-        .label_addr = undefined,
-        .tokens = undefined,
-        .codes = undefined,
-        .src_file_buf = undefined,
-        .out_buf = undefined,
     };
 }
 
 // *const [][:0]u8 = pointer to an array of zero terminated const u8 values
 pub fn parseArgs(self: *ImFineAssembler, args_p: *const [][:0]u8) !void {
+    _ = self;
     if (args_p.len < 2)
         return ArgError.TooFewArgs;
 
     for (args_p.*, 0..) |arg, i| {
+        _ = arg;
         if (i == 0) continue;
-        if (eql(u8, arg, "--debug")) {
-            DEBUG = true;
-            continue;
-        }
+        //if (eql(u8, arg, "--debug")) {
+        //    DEBUG = true;
+        //    continue;
+        //}
         // arg is file name
-        copyForwards(u8, &file_name_buf, arg);
-        self.src_name = arg;
+        // copyForwards(u8, &file_name_buf, arg);
+        //self.src_name = arg;
     }
-    self.assignOutputName();
+    //self.assignOutputName();
 }
 
 fn nameEndWithAsm(file_name: []u8) bool {
@@ -122,229 +62,6 @@ fn nameEndWithAsm(file_name: []u8) bool {
         file_name[length - 4 .. length],
         ".asm",
     );
-}
-
-fn assignOutputName(self: *ImFineAssembler) void {
-    const extension = ".bin";
-    const src_name_len = self.src_name.len;
-
-    if (nameEndWithAsm(self.src_name)) {
-        copyForwards(
-            u8,
-            file_name_buf[src_name_len - 4 .. src_name_len],
-            extension,
-        );
-        self.dst_name = file_name_buf[0..src_name_len];
-    } else {
-        copyForwards(
-            u8,
-            file_name_buf[src_name_len - 1 .. src_name_len + 3],
-            extension,
-        );
-        self.dst_name = file_name_buf[0 .. src_name_len + extension.len];
-    }
-}
-
-pub fn getLen(num: u128) u3 {
-    return switch (num) {
-        0 => 0b000,
-        1...(pow(u16, 2, 8) - 1) => 0b001,
-        pow(u16, 2, 8)...(pow(u32, 2, 16) - 1) => 0b010,
-        pow(u32, 2, 16)...(pow(u64, 2, 32) - 1) => 0b011,
-        pow(u64, 2, 32)...(pow(u128, 2, 64) - 1) => 0b100,
-        pow(u128, 2, 64)...(pow(u129, 2, 128) - 1) => 0b100,
-    };
-}
-
-/// remove indent
-/// or brank line
-fn preProcess(buf: []u8) !usize {
-    const allocator = std.heap.page_allocator;
-    const tmp = try allocator.alloc(u8, buf.len);
-    defer allocator.free(tmp);
-
-    var i: usize = 0;
-    var newline: bool = false;
-    var comment: bool = false;
-    clean: for (buf) |char| {
-        if (comment) {
-            if (char == LINE_DELIMITER) {
-                comment = false;
-            } else {
-                continue :clean;
-            }
-        }
-
-        if (newline) {
-            if (char == LINE_DELIMITER) {
-                continue :clean;
-            }
-        }
-
-        switch (char) {
-            INDENT => continue :clean,
-            LINE_DELIMITER => newline = true,
-            COMMENT_START => {
-                comment = true;
-                continue :clean;
-            },
-            else => newline = false,
-        }
-
-        tmp[i] = char;
-        i += 1;
-    }
-    i -= 1;
-
-    @memset(tmp[i..tmp.len], '\x00');
-    @memset(buf, '\x00');
-    mem.copyForwards(u8, buf, tmp);
-
-    return i;
-}
-
-/// check if syntax is ok
-fn validate(buf: []u8) !void {
-    const allocator = std.heap.page_allocator;
-    const tmp = try allocator.alloc(u8, buf.len);
-    defer allocator.free(tmp);
-
-    var buf_idx: usize = 0;
-    while (nextLine(buf, &buf_idx)) |line| {
-        try validateToken(line);
-    }
-}
-
-const ValidateState = struct {};
-
-fn isRegister(buf: []const u8) bool {
-    for (registers) |register|
-        if (eql(u8, buf, register))
-            return true;
-    return false;
-}
-
-fn isOpcode(buf: []const u8) bool {
-    for (instructions) |instruction|
-        if (eql(u8, buf, instruction))
-            return true;
-    return false;
-}
-
-fn isImm(buf: []const u8) bool {
-    var res: ?u128 = 0;
-    for ([2]u8{ 10, 16 }) |base| {
-        if (buf.len > 2 and
-            buf[0] == '0' and
-            buf[1] == 'x')
-        {
-            res = std.fmt.parseInt(u128, buf[2..buf.len], base) catch null;
-        } else res = std.fmt.parseInt(u128, buf, base) catch null;
-        if (base == 16) {
-            if (res == null) {
-                return false;
-            } else return true;
-        }
-    }
-    return false;
-}
-
-fn isRef(buf: []const u8) bool {
-    if (buf[0] == SQUARE_BRACKET_OPEN)
-        return true;
-    return false;
-}
-
-fn isLabel(buf: []const u8) bool {
-    if (buf[buf.len - 1] == COLON)
-        return true;
-    return false;
-}
-fn validateToken(line: []u8) !void {
-    var label: bool = false;
-    var second_oprand_expected: bool = false;
-
-    var line_idx: usize = 0;
-    while (readToken(line, &line_idx)) |token| {
-        var new_token: []u8 = token;
-
-        if (label) {
-            if (readToken(line, &line_idx)) |invalid_token| {
-                debugPrint("invalid token -> %s\n", .{invalid_token});
-                return;
-            }
-        }
-
-        if (second_oprand_expected) {
-            if (!readToken(line, &line_idx)) {
-                debugPrint("token expected -> %s _\n", .{token});
-                return;
-            }
-            second_oprand_expected = false;
-        }
-
-        // if token is label
-        if (endWithColon(token)) {
-            label = true;
-            continue;
-        }
-
-        if (endWithComma(token)) {
-            second_oprand_expected = true;
-            new_token = removeComma(token);
-        }
-
-        // for ()
-    }
-}
-
-inline fn removeComma(token: []u8) []u8 {
-    return token[0 .. token.len - 2];
-}
-
-inline fn endWithColon(token: []u8) bool {
-    return token[token.len - 1] == COLON;
-}
-
-inline fn endWithComma(token: []u8) bool {
-    return token[token.len - 1] == COMMA;
-}
-
-fn readToken(line: []u8, idx: *usize) ?[]u8 {
-    const start = idx.*;
-    const end = for (line) |char| {
-        switch (char) {
-            SPACE => {
-                defer idx.* += 1;
-                break idx.*;
-            },
-            COMMA => {
-                idx.* += 1;
-                break idx.*;
-            },
-            COLON => {
-                idx.* += 1;
-                break idx.*;
-            },
-            else => {},
-        }
-        idx.* += 1;
-    };
-    const token = line[start..end];
-
-    return if (token.len == 0) null else token;
-}
-
-fn nextLine(buf: anytype, idx: *usize) ?[]u8 {
-    const start = idx.*;
-    const end = for (buf[start..buf.len]) |char| {
-        if (char == LINE_DELIMITER)
-            break idx.*;
-        idx.* += 1;
-    } else buf.len;
-    const line = buf[start..end];
-
-    return if (line.len == 0) null else line;
 }
 
 const instructions = [_][]const u8{
@@ -381,68 +98,170 @@ const registers = [_][]const u8{
 
 fn isDigit(ch: u8) bool {
     const code = @as(i8, @intCast(ch));
-    return code - '0' < 10 and code - '0' >= 0;
+    return 0 <= code - '0' and code - '0' < 10;
 }
 
 fn isHex(ch: u8) bool {
     const code = @as(i8, @intCast(ch));
-    return (code - 'a' < 6 and code - 'a' >= 0) or isDigit(ch);
+    return (0 <= code - 'a' and code - 'a' < 6) or isDigit(ch);
 }
 
-var index: usize = 0;
-fn nextChar(buf: []const u8) u8 {
-    defer index += 1;
-    return buf[index];
+fn isLetter(ch: u8) bool {
+    const code = @as(i8, @intCast(ch));
+    return (0 <= code - 'a' and code - 'a' < 26) or (0 <= code - 'A' and code - 'A' < 26);
+}
+
+fn nextChar(reader: anytype) u8 {
+    return reader.readByte() catch 0;
+}
+
+const TokenKind = enum {
+    opcode,
+    //immediate,
+    register,
+    //reference,
+    label,
+    labelDecl,
+    decLiteral,
+    hexLiteral,
+};
+
+//fn tokenValType(comptime T: type) type {
+//    return switch (@typeInfo(T)) {
+//        .Int => u32,
+//        else => struct {
+//            ident: []u8,
+//            _buf: [MAX_IDENT_LEN]u8,
+//        },
+//    };
+//}
+
+const Token = struct {
+    kind: TokenKind,
+    val: union {
+        num: u32,
+        _buf: [MAX_IDENT_LEN]u8,
+    },
+    ident: []u8,
+
+    //pub fn init() Token {
+    //    return .{
+    //        .kind = undefined,
+    //        .val = undefined,
+    //    };
+    //}
+};
+
+const CharKind = enum {
+    digit,
+    letter,
+    other,
+
+    pub fn get(ch: u8) CharKind {
+        return switch (ch) {
+            'a'...'z' => CharKind.letter,
+            '0'...'9' => CharKind.digit,
+            else => CharKind.other,
+        };
+    }
+};
+
+fn charClass(ch: u8) CharKind {
+    switch (ch) {
+        'a'...'z' => {},
+        '0'...'9' => {},
+        ':' => {},
+        else => {},
+    }
+}
+
+const MAX_IDENT_LEN = 0x20;
+// TODO: limit skipping whitespaces by setting max spaces
+fn nextToken(reader: anytype) Token {
+    var ch = nextChar(reader);
+    var kind: TokenKind = undefined;
+    var num: u32 = 0;
+    var buf: [MAX_IDENT_LEN]u8 = [_]u8{0} ** MAX_IDENT_LEN;
+    var i: usize = 0;
+
+    // skip whitespace or indent
+    while (ch == ' ' or ch == '\t')
+        ch = nextChar(reader);
+
+    switch (CharKind.get(ch)) {
+        .digit => {
+            kind = .decLiteral;
+            if (ch == '0') {
+                i += 1;
+                ch = nextChar(reader);
+
+                if (ch == 'x') {
+                    i += 1;
+                    ch = nextChar(reader);
+                    kind = .hexLiteral;
+                    while (isHex(ch)) {
+                        num = num * 16 + (ch - '0');
+                        i += 1;
+                        ch = nextChar(reader);
+                    }
+                }
+            } else {
+                while (isDigit(ch)) {
+                    num = num * 10 + (ch - '0');
+                    i += 1;
+                    ch = nextChar(reader);
+                }
+            }
+        },
+        .letter => {
+            buf[i] = ch;
+            i += 1;
+            ch = nextChar(reader);
+
+            while ((isLetter(ch) or isDigit(ch)) and i < MAX_IDENT_LEN) {
+                buf[i] = ch;
+                i += 1;
+                ch = nextChar(reader);
+            }
+        },
+        else => {
+            @panic("baka");
+        },
+    }
+
+    return Token{
+        .kind = kind,
+        .ident = buf[0..i],
+        .val = switch (kind) {
+            .decLiteral, .hexLiteral => .{ .num = num },
+            else => .{ ._buf = buf },
+        },
+    };
+}
+
+fn memoryReference() void {}
+
+fn instruction() void {}
+// fn label() void {}
+fn program() void {
+    instruction();
 }
 
 fn assemble(self: *ImFineAssembler) !void {
     const src_file = try fs.cwd().openFile(self.src_name, .{ .mode = .read_only });
+    defer src_file.close();
+    const src_reader = src_file.reader();
+    _ = nextChar(src_reader);
 
-    self.src_file_buf = try zig.readSourceFileToEndAlloc(self.allocator, src_file, null);
-    defer self.allocator.free(self.src_file_buf);
-
-    src_file.close();
-
-    const length = try preProcess(self.src_file_buf);
-    _ = length;
-
-    debugPrint("{s}", .{self.src_file_buf});
-
-    // const dst_file = try fs.cwd().createFile(self.dst_name, .{});
-    // defer dst_file.close();
-
-    // const output_writer = dst_file.writer();
-
-    // self.tokens = ArrayList(?[]u8).init(self.allocator);
-    // defer self.tokens.deinit();
-
-    // self.codes = ArrayList(Code).init(self.allocator);
-    // defer self.codes.deinit();
-
-    // self.label_addr = ArrayList(Label2addr).init(self.allocator);
-    // defer self.label_addr.deinit();
-
-    // self.out_buf = ArrayList(u8).init(self.allocator);
-    // defer self.out_buf.deinit();
-
-    // var tokenizer = Tokenizer.init(self);
-    // try tokenizer.tokenize();
-
-    // var parser = Parser.init(self);
-    // try parser.parse();
-
-    // var encoder = Encoder.init(self);
-    // try encoder.encode();
-
-    // try output_writer.writeAll(self.out_buf.items);
+    program();
 }
 
 pub fn entry(self: *ImFineAssembler) void {
     self.assemble() catch |err| {
         debugPrint("{!}\n", .{err});
-        fs.cwd().deleteFile(
-            self.dst_name,
-        ) catch return;
+        //fs.cwd().deleteFile(
+        //    self.dst_name,
+        //) catch return;
     };
 }
 
@@ -466,4 +285,70 @@ test "hex" {
     try std.testing.expect(!isHex('/'));
     try std.testing.expect(!isHex('`'));
     try std.testing.expect(!isHex('g'));
+}
+
+test "letter" {
+    for ('A'..'Z' + 1) |letter|
+        try std.testing.expect(isLetter(@intCast(letter)));
+    for ('a'..'z' + 1) |letter|
+        try std.testing.expect(isLetter(@intCast(letter)));
+
+    const failcase = [_]u8{ ':', '.', '`', '_' };
+    for (failcase) |letter|
+        try std.testing.expect(!isLetter(@intCast(letter)));
+}
+
+test "number literal" {
+    const fbs = std.io.fixedBufferStream;
+
+    {
+        const literal = "0x10 ";
+        var stream = fbs(literal);
+        const reader = stream.reader();
+        const token = nextToken(reader);
+        try std.testing.expect(token.kind == .hexLiteral and token.val.num == 0x10);
+    }
+    {
+        const literal = "0x10";
+        var stream = fbs(literal);
+        const reader = stream.reader();
+        const token = nextToken(reader);
+        try std.testing.expect(token.kind == .hexLiteral and token.val.num == 0x10);
+    }
+    {
+        const literal = "0";
+        var stream = fbs(literal);
+        const reader = stream.reader();
+        const token = nextToken(reader);
+        try std.testing.expect(token.kind == .decLiteral and token.val.num == 0);
+    }
+    {
+        const literal = "10";
+        var stream = fbs(literal);
+        const reader = stream.reader();
+        const token = nextToken(reader);
+        try std.testing.expect(token.kind == .decLiteral and token.val.num == 10);
+    }
+    {
+        const literal = "20000";
+        var stream = fbs(literal);
+        const reader = stream.reader();
+        const token = nextToken(reader);
+        try std.testing.expect(token.kind == .decLiteral and token.val.num == 20000);
+    }
+}
+
+test "identifier" {
+    const fbs = std.io.fixedBufferStream;
+    {
+        const ident = "hoge";
+        //const hoge_slice: []const u8 = ident;
+        //try std.testing.expectEqualStrings(ident, hoge_slice);
+
+        var stream = fbs(ident);
+        const reader = stream.reader();
+        const token = nextToken(reader);
+        try std.testing.expect(token.kind == .label);
+        try std.testing.expectEqualStrings(ident, token.ident);
+    }
 }
